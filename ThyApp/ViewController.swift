97c9fa92f13638b113d63d9ef88087b3bdb4f051
Le Xuan Quynh <le-xuan-quynh@goodcycle.net>
2023-12-19 16:04:47 +0700

    first init

---
+++ ThyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThyApp
+//
+//  Created by Le Xuan Quynh on 19/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
